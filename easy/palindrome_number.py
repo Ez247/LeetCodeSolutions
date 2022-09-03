63c153b489e465239faa0ef176d2e13482444bf4
J L <94430459+Ez247@users.noreply.github.com>
2022-09-03 13:45:06 -0400

    Update palindrome_number.py

--- easy/palindrome_number.py
+++ easy/palindrome_number.py
@@ -1,6 +1,5 @@
 """Given an integer x, return true if x is palindrome integer"""
 
-
 def isPalindrome(self, x: int) -> bool:
     x = str(x)           # convert x to a string
     return x == x[::-1]  # reverse x to read the same backwards
